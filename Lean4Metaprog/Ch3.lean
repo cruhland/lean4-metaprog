aa18b258601a89428263394bf20b3845853650c4
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-08-19 20:58:58 -0700

    Ch3: Constructing expressions - constants

--- Lean4Metaprog/Ch3.lean
+++ Lean4Metaprog/Ch3.lean
@@ -1,3 +1,5 @@
+import Lean
+
 namespace Lean4Metaprog.Ch3
 
 /-! # Chapter 3: Expressions -/
@@ -6,5 +8,32 @@ namespace Lean4Metaprog.Ch3
 
 set_option pp.universes true in
 #check @List.map
+
+/-! ## Constructing expressions -/
+
+open Lean
+
+def z' := Expr.const `Nat.zero []
+#eval z'
+
+def z := Expr.const ``Nat.zero []
+#eval z
+
+section resolve
+
+open Nat
+
+def z₁ := Expr.const `zero []
+#eval z₁
+
+-- Without `open Nat`, gives error "unknown constant 'zero'"
+def z₂ := Expr.const ``zero []
+#eval z₂
+
+-- Shorter way to create constants
+def z₃ := mkConst ``zero
+#eval z₃
+
+end resolve
 
 end Lean4Metaprog.Ch3
