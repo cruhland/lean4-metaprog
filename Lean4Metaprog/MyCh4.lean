211d09680f0c23ecd6fcc16eaabed023db99df83
Charles Ruhland <cruhland@alumni.cmu.edu>
2025-03-20 06:39:14 -0700

    MyCh4: metavariable assignment example (WIP)

--- Lean4Metaprog/MyCh4.lean
+++ Lean4Metaprog/MyCh4.lean
@@ -15,9 +15,43 @@ namespace Lean4Metaprog.Ch4
 
 -- Assign an expression to a metavariable (unsafely)
 #check Lean.MVarId.assign
+-- MyMetaM equivalents not needed yet
 
 -- Retrieve information about a metavariable
 #check Lean.MVarId.getDecl -- throws exception (in the monad) if not found
 #check Lean.MVarId.findDecl? -- returns Option
+#check Lean.MVarId.getType -- only get the type (throws exception)
+-- MyMetaM equivalents not needed yet
+
+-- Retrieve or check metavariable assignment; rarely used
+#check Lean.getExprMVarAssignment?
+#check Lean.MVarId.isAssigned
+-- MyMetaM equivalents not needed yet
+
+-- Replaces all assigned metavariables in expression with their assignments
+#check Lean.instantiateMVars
+-- MyMetaM equivalents not needed yet
+
+#eval show Lean.MetaM Unit from do
+  let natTy := Lean.Expr.const ``Nat []
+  -- Create two fresh metavariables of type `Nat`.
+  let mvar1 ← Lean.Meta.mkFreshExprMVar natTy (userName := `mvar1)
+  let mvar2 ← Lean.Meta.mkFreshExprMVar natTy (userName := `mvar2)
+
+  /-
+  Create a fresh metavariable of type `Nat → Nat`. The `mkArrow` function
+  creates a function type.
+  -/
+  let natFn ← Lean.mkArrow natTy natTy
+  let mvar3 ← Lean.Meta.mkFreshExprMVar natFn (userName := `mvar3)
+
+  -- Define a helper function that prints each metavariable.
+  let printMVars : Lean.MetaM Unit := do
+    IO.println s!"  meta1: {← Lean.instantiateMVars mvar1}"
+    IO.println s!"  meta2: {← Lean.instantiateMVars mvar2}"
+    IO.println s!"  meta3: {← Lean.instantiateMVars mvar3}"
+
+  IO.println "Initially, all metavariables are unassigned:"
+  printMVars
 
 end Lean4Metaprog.Ch4
--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
 On `generic` branch:
+1. Finish metavariable assignment example in `MyCh4`.
+1. Update mvar assignment example with your own definitions.
 1. Make `MyMetaM MetaM` instance more efficient by replacing operations with
    more low-level ones.
 1. Finish Chapter 4
