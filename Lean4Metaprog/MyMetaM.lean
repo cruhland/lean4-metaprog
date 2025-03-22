26810b4ad73a9b4eaa1f701074cbaef5c6b8a5ae
Charles Ruhland <cruhland@alumni.cmu.edu>
2025-03-21 18:21:07 -0700

    MyCh4: replace Lean.mkArrow with MyMetaM.mkArrow

--- Lean4Metaprog/MyMetaM.lean
+++ Lean4Metaprog/MyMetaM.lean
@@ -4,7 +4,7 @@ import Lean4Metaprog.MyMVarId
 namespace Lean4Metaprog
 
 /-- The parts of the `Lean.MetaM` interface that I've used so far. -/
-class MyMetaM (M : Type → Type) where
+class MyMetaM (M : Type → Type) extends Monad M where
   /-- The type of unique identifiers for metavariables. -/
   MVarId : Type
 
@@ -14,17 +14,30 @@ class MyMetaM (M : Type → Type) where
   /-- Create a new, unique metavariable with the given type. -/
   mkFreshMVar {E : Type} [MyExpr E] (type : E) : M MVarId
 
+  /-- Create a new, unique name based on the given user-friendly name. -/
+  mkFreshUserName {N : Type} [MyName N] (name : N) : M Lean.Name
+
 instance mymetam_metam_inst : MyMetaM Lean.MetaM := {
   MVarId := Lean.MVarId
   myMVarId := inferInstance
   mkFreshMVar := λ type => do
     let exprMVar ← Lean.Meta.mkFreshExprMVar (MyExpr.toExpr type)
     return exprMVar.mvarId!
+  mkFreshUserName := λ name => Lean.Core.mkFreshUserName (MyName.toName name)
 }
 
-instance mymvarid_mymetam_mvarid_inst
-    {M : Type → Type} [MyMetaM M] : MyMVarId (MyMetaM.MVarId M)
-    :=
+namespace MyMetaM
+
+variable {M : Type → Type} [MyMetaM M]
+
+instance mymvarid_mymetam_mvarid_inst : MyMVarId (MyMetaM.MVarId M) :=
   MyMetaM.myMVarId
 
-end Lean4Metaprog
+/--
+Creates an expression for a non-dependent function type, i.e.
+`argType → bodyType`.
+-/
+def mkArrow {E : Type} [MyExpr E] (argType bodyType : E) : M E :=
+  return MyExpr.forallE (← mkFreshUserName `x) argType bodyType
+
+end Lean4Metaprog.MyMetaM
--- TODO.md
+++ TODO.md
@@ -1,8 +1,9 @@
 On `generic` branch:
-1. Use your own `mkArrow`
 1. Use your own `instantiateMVars`
 1. Use your own `assign`
+1. Replace `Lean.Expr` type ascription with generic `E`
 1. Update mvar assignment example with your own definitions.
+1. Have `MyMetaM` just use `Lean.MVarId` for return values, not its own field
 1. Make `MyMetaM MetaM` instance more efficient by replacing operations with
    more low-level ones.
 1. Finish Chapter 4
