7ba982c23ac34349c9cfd10845fb77814af55157
Charles Ruhland <cruhland@alumni.cmu.edu>
2025-05-22 06:03:02 -0700

    Ch5: syntax combinators section

--- Lean4Metaprog/Ch5.lean
+++ Lean4Metaprog/Ch5.lean
@@ -69,4 +69,30 @@ syntax:50 boolean_expr " AND " boolean_expr : boolean_expr
 syntax "[Bool|" boolean_expr "]" : term
 #check_failure [Bool| ⊥ AND ⊤ ]
 
+/-! ### Syntax combinators -/
+
+syntax binOne := "O"
+syntax binZero := "Z"
+syntax binDigit := binZero <|> binOne
+
+-- The `+` denotes "one or more"; use `*` for "zero or more"
+-- The `,` is the separator between items; if omitted, space is the separator
+syntax binNumber := binDigit,+
+
+syntax "bin(" binNumber ")" : term
+#check_failure bin(Z, O, Z, Z, O) -- no elab, but parsing succeeds
+-- #check_failure bin() -- fails to parse, need at least one O or Z
+
+syntax binNumber' := binDigit,*
+syntax "emptyBin(" binNumber' ")" : term
+#check_failure emptyBin() -- no elab, but parsing succeeds
+
+syntax "binCompact(" ("Z" <|> "O"),+ ")" : term
+#check_failure binCompact(Z, O, Z, Z, O) -- no elab, but parsing succeeds
+
+-- Write `(...)?` to denote an optional portion
+syntax "binDoc(" (str ";")? binNumber ")" : term
+#check_failure binDoc(Z, O, Z, Z, O) -- no elab, valid parse
+#check_failure binDoc("mycomment"; Z, O, Z, Z, O) -- no elab, valid parse
+
 end Lean4Metaprog.Ch5
