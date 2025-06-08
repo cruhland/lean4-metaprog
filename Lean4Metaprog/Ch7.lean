61f0ef8a61033b6a9f59862b6ab60e6f92a4f532
Charles Ruhland <cruhland@alumni.cmu.edu>
2025-06-08 10:18:46 -0700

    Ch7: making our own command elab

--- Lean4Metaprog/Ch7.lean
+++ Lean4Metaprog/Ch7.lean
@@ -24,4 +24,41 @@ namespace Lean4Metaprog.Ch7
 
 #check Lean.throwError
 
+/-! ### Making our own -/
+
+open Lean.Elab.Command (CommandElab)
+
+syntax (name := mycommand1) "#mycommand1" : command
+
+@[command_elab mycommand1]
+def myCommand1Impl : CommandElab := λ _ => Lean.logInfo "Hello world"
+
+#mycommand1 -- Hello world
+
+elab "#mycommand2" : command => Lean.logInfo "Hello world"
+
+#mycommand2 -- Hello world
+
+@[command_elab mycommand1]
+def myNewImpl : CommandElab := λ _ => Lean.logInfo "new!"
+
+#mycommand1 -- new!
+
+elab "#check" "mycheck" : command => Lean.logInfo "got ya!"
+
+#check mycheck -- got ya!
+#check "Hello" -- "Hello" : String
+#check Nat.add -- Nat.add : Nat → Nat → Nat
+
+@[command_elab Lean.Parser.Command.check] def mySpecialCheck : CommandElab :=
+  λ stx => do
+    if let some str := stx[1].isStrLit? then
+      Lean.logInfo s!"Special elab of string literal: {str} : String"
+    else
+      Lean.Elab.throwUnsupportedSyntax
+
+#check mycheck -- got ya!
+#check "Hello" -- Special elab of string literal: Hello : String
+#check Nat.add -- Nat.add : Nat → Nat → Nat
+
 end Lean4Metaprog.Ch7
